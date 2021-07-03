44ea463f65accf0cf6a7efa83b537048a280949f
RubySpideybot <86848301+RubySpideybot@users.noreply.github.com>
2021-07-03 11:23:59 +0530

    Update muting.py

--- tg_bot/modules/muting.py
+++ tg_bot/modules/muting.py
@@ -158,7 +158,7 @@ def temp_mute(bot: Bot, update: Update, args: List[str]) -> str:
     try:
         if member.can_send_messages is None or member.can_send_messages:
             bot.restrict_chat_member(chat.id, user_id, until_date=mutetime, can_send_messages=False)
-            message.reply_text("Shut up 😠, {} is Muted for {}!".format(member.user.first_name),time_val)
+            message.reply_text("Shut up 😠, {} is Muted for {}!".format(member.user.first_name,time_val))
             return log
         else:
             message.reply_text("This user is already muted.")
@@ -166,7 +166,7 @@ def temp_mute(bot: Bot, update: Update, args: List[str]) -> str:
     except BadRequest as excp:
         if excp.message == "Reply message not found":
             # Do not reply
-            message.reply_text("Shut up 😠, {} is Muted for {}!".format(member.user.first_name), time_val), quote=False)
+            message.reply_text("Shut up 😠, {} is Muted for {}!".format(member.user.first_name, time_val), quote=False)
             return log
         else:
             LOGGER.warning(update)
