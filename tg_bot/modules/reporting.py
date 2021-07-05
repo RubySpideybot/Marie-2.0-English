b88862545141c9dc33eb9666183503a9943d4edf
RubySpideybot <86848301+RubySpideybot@users.noreply.github.com>
2021-07-05 15:40:06 +0530

    Update reporting.py

--- tg_bot/modules/reporting.py
+++ tg_bot/modules/reporting.py
@@ -6,7 +6,7 @@ from telegram.error import BadRequest, Unauthorized
 from telegram.ext import CommandHandler, RegexHandler, Filters, run_async
 from telegram.utils.helpers import mention_html
 
-from tg_bot import dispatcher, LOGGER, SUDO_USERS, TIGER_USERS
+from tg_bot import dispatcher, LOGGER
 from tg_bot.modules.helper_funcs.chat_status import user_not_admin, user_admin
 from tg_bot.modules.log_channel import loggable
 from tg_bot.modules.sql import reporting_sql as sql
